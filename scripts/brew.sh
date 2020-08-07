4b1e98e916622a098cb496378ad81f0923260e5e
Beach Linnea <blinnea@oa-n5.msk.21-school.ru>
2020-08-07 14:06:07 +0300

    Testing branching system

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -6,7 +6,7 @@
 #    By: blinnea <blinnea@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/05 11:44:34 by blinnea           #+#    #+#              #
-#    Updated: 2020/08/05 19:59:26 by blinnea          ###   ########.fr        #
+#    Updated: 2020/08/07 13:52:00 by blinnea          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,56 +16,50 @@
 IPATH=/goinfre/$USER/
 mkdir -p $IPATH
 
-# Delete and reinstall Homebrew from Github repo
-answer='Y'
-if (( $+commands[brew] ))
+# Install Brew if It isn't here
+if ! command -v brew
 then
-	answer='N'
-	vared -p 'Homebrew is already installed. Would you like to reinstall it? y/N: ' -c answer
-fi
-if [[ $answer == 'Y' || $answer == 'y' ]]
-then
-	echo "the answer is yes"
+	# Remove all posible old directories
 	rm -rf $HOME/.brew
 	rm -rf $IPATH/.brew
+	# Clone repositorie from github
 	git clone --depth=1 https://github.com/Homebrew/brew $IPATH/.brew
-
 	# Create .brewconfig script in home directory
 	cat > $IPATH/.brewconfig.zsh <<EOL
-# HOMEBREW CONFIG
+		# HOMEBREW CONFIG
 
-# Add brew to path
-export PATH=$IPATH/.brew/bin:\$PATH
+		# Add brew to path
+		export PATH=$IPATH/.brew/bin:\$PATH
 
-# Set Homebrew temporary folders
-export HOMEBREW_CACHE=/tmp/\$USER/Homebrew/Caches
-export HOMEBREW_TEMP=/tmp/\$USER/Homebrew/Temp
+		# Set Homebrew temporary folders
+		export HOMEBREW_CACHE=/tmp/\$USER/Homebrew/Caches
+		export HOMEBREW_TEMP=/tmp/\$USER/Homebrew/Temp
 
-mkdir -p \$HOMEBREW_CACHE
-mkdir -p \$HOMEBREW_TEMP
+		mkdir -p \$HOMEBREW_CACHE
+		mkdir -p \$HOMEBREW_TEMP
 
-# If NFS session
-# Symlink Locks folder in /tmp
-if df -T autofs,nfs \$IPATH 1>/dev/null
-then
-  HOMEBREW_LOCKS_TARGET=/tmp/\$HOME/Homebrew/Locks
-  HOMEBREW_LOCKS_FOLDER=$IPATH/.brew/var/homebrew
+		# If NFS session
+		# Symlink Locks folder in /tmp
+		if df -T autofs,nfs \$IPATH 1>/dev/null
+		then
+		HOMEBREW_LOCKS_TARGET=/tmp/\$HOME/Homebrew/Locks
+		HOMEBREW_LOCKS_FOLDER=$IPATH/.brew/var/homebrew
 
-  mkdir -p \$HOMEBREW_LOCKS_TARGET
-  mkdir -p \$HOMEBREW_LOCKS_FOLDER
+		mkdir -p \$HOMEBREW_LOCKS_TARGET
+		mkdir -p \$HOMEBREW_LOCKS_FOLDER
 
-  # Symlink to Locks target folders
-  # If not already a symlink
-  if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]
-  then
-     echo "Creating symlink for Locks folder"
-     rm -rf \$HOMEBREW_LOCKS_FOLDER
-     ln -s \$HOMEBREW_LOCKS_TARGET \$HOMEBREW_LOCKS_FOLDER
-  fi
-fi
+		# Symlink to Locks target folders
+		# If not already a symlink
+		if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]
+		then
+			echo "Creating symlink for Locks folder"
+			rm -rf \$HOMEBREW_LOCKS_FOLDER
+			ln -s \$HOMEBREW_LOCKS_TARGET \$HOMEBREW_LOCKS_FOLDER
+		fi
+		fi
 EOL
 
-	# Add .brewconfig sourcing in your .zshrc if not already present
+	# Добавить файл .brewconfig в ваш .zshrc, если он ещё не там
 	if ! grep -q "# Load Homebrew config script" $HOME/.zshrc
 	then
 	cat >> $HOME/.zshrc <<EOL
---
+++ src/brewconfig.zsh
@@ -0,0 +1,43 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    .brewconfig.zsh                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: blinnea <blinnea@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/08/07 13:53:40 by blinnea           #+#    #+#              #
+#    Updated: 2020/08/07 13:53:59 by blinnea          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# HOMEBREW CONFIG
+
+# Add brew to path
+export PATH=$IPATH/.brew/bin:\$PATH
+
+# Set Homebrew temporary folders
+export HOMEBREW_CACHE=/tmp/\$USER/Homebrew/Caches
+export HOMEBREW_TEMP=/tmp/\$USER/Homebrew/Temp
+
+mkdir -p \$HOMEBREW_CACHE
+mkdir -p \$HOMEBREW_TEMP
+
+# If NFS session
+# Symlink Locks folder in /tmp
+if df -T autofs,nfs \$IPATH 1>/dev/null
+then
+HOMEBREW_LOCKS_TARGET=/tmp/\$HOME/Homebrew/Locks
+HOMEBREW_LOCKS_FOLDER=$IPATH/.brew/var/homebrew
+
+mkdir -p \$HOMEBREW_LOCKS_TARGET
+mkdir -p \$HOMEBREW_LOCKS_FOLDER
+
+# Symlink to Locks target folders
+# If not already a symlink
+if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]
+then
+	echo "Creating symlink for Locks folder"
+	rm -rf \$HOMEBREW_LOCKS_FOLDER
+	ln -s \$HOMEBREW_LOCKS_TARGET \$HOMEBREW_LOCKS_FOLDER
+fi
+fi
