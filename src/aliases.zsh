52c69ac6dd80e20e135dde423a2b305df026f436
Beach Linnea <blinnea@mi-n3.msk.21-school.ru>
2020-08-07 18:52:12 +0300

    aliases added

---
+++ src/aliases.zsh
@@ -0,0 +1,10 @@
+# My own aliases
+alias vscode="open -a 'Visual Studio Code'"
+alias chrome="open -a 'Google Chrome'"
+alias telegram="open -a 'Telegram'"
+alias python=python3
+alias bi="brew install"
+alias c=curl
+alias -s {sh,c,h}=vscode
+alias -s {html, gif}=chrome
+alias gfind="web_search Google"
